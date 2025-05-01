4af5a7e3c409ebc1f479ca72d7f27ec9f477eaf9
jonasluis <jonasluis66@gmail.com>
2025-05-01 12:04:34 -0300

    feat: Post data into database

--- frontend/src/users/AddUser.js
+++ frontend/src/users/AddUser.js
@@ -1,50 +1,79 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddUser() {
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { name, username, email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center mb-4">Add User</h2>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              name="name"
-              placeholder="Enter your name"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              name="username"
-              placeholder="Enter your username"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              Email
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              name="email"
-              placeholder="Enter your email"
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger">
-            Cancel
-          </button>
+          <form onSubmit={onSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="name"
+                placeholder="Enter your name"
+                value={name}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="username"
+                placeholder="Enter your username"
+                value={username}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                Email
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={onInputChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link  className="btn btn-outline-danger" to='/'>
+              Cancel
+            </Link>
+          </form>
         </div>
       </div>
     </div>
